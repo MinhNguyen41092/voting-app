26fdada981a8b8c048706b733e60330e5d147d52
Nguyen Dinh Minh <minh.nguyendinh@asnet.com.vn>
2018-02-09 15:09:25 +0700

    display list of products

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import Seed from './seed';
 
 class ProductList extends Component {
   render() {
-    const product = Seed.products[0];
+    const productComponents = Seed.products.map(product => (
+      <Product
+        key={`product-${product.id}`}
+        id={product.id}
+        title={product.title}
+        description={product.description}
+        url={product.url}
+        votes={product.votes}
+        submitterAvatarUrl={product.submitterAvatarUrl}
+        productImageUrl={product.productImageUrl}
+      />
+    ));
+    
     return (
       <div className='ui unstackable items'>
-        <Product
-          id={product.id}
-          title={product.title}
-          description={product.description}
-          url={product.url}
-          votes={product.votes}
-          submitterAvatarUrl={product.submitterAvatarUrl}
-          productImageUrl={product.productImageUrl}
-        />
+        {productComponents}
       </div>
     );
   }
