e970a880178fd98d8d3b93d1e8e8d053fa72646b
Nguyen Dinh Minh <minh.nguyendinh@asnet.com.vn>
2018-02-09 10:26:09 +0700

    First component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react';
+import logo from './logo.svg';
+
+class ProductList extends Component {
+  render() {
+    return (
+      <div className='ui unstackable items'>
+        Hello, friend! I am a basic React component.
+      </div>
+    );
+  }
+}
+
+export default ProductList;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ProductList from './App';
+
+ReactDOM.render(
+  <ProductList />,
+  document.getElementById('content')
+);
